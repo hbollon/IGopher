60c1c3b386be28f30403176c489f0927826e9dcb
Hugo Bollon <hugo.bollon@gmail.com>
2020-11-23 19:29:11 +0100

    WebDriver initialization and deferred Selenium close

--- app.go
+++ app.go
@@ -1,7 +1,5 @@
 package main
 
-import "github.com/hbollon/go-instadm/lib"
-
 // WebDriver is the main Selenium instance used by this bot
 var WebDriver Selenium
 
@@ -10,5 +8,7 @@ func main() {
 	lib.DownloadDependencies(true, true)
 
 	// var webDriver Selenium
-	WebDriver.InitializeWebDriver()
+	WebDriver.InitializeSelenium()
+	WebDriver.InitFirefoxWebDriver()
+	defer WebDriver.CloseSelenium()
 }
--- engine.go
+++ engine.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	log "github.com/sirupsen/logrus"
@@ -13,15 +14,18 @@ const (
 	port            = 8080
 )
 
+var err error
+
 // Selenium instance and opts
 type Selenium struct {
-	Instance selenium.Service
-	Opts     []selenium.ServiceOption
+	Instance  *selenium.Service
+	Opts      []selenium.ServiceOption
+	WebDriver selenium.WebDriver
 }
 
-// InitializeWebDriver start a Selenium WebDriver server instance
+// InitializeSelenium start a Selenium WebDriver server instance
 // (if one is not already running).
-func (s *Selenium) InitializeWebDriver() {
+func (s *Selenium) InitializeSelenium() {
 	s.Opts = []selenium.ServiceOption{
 		selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
 		selenium.GeckoDriver(geckoDriverPath), // Specify the path to GeckoDriver in order to use Firefox.
@@ -29,9 +33,25 @@ func (s *Selenium) InitializeWebDriver() {
 	}
 
 	selenium.SetDebug(true)
-	service, err := selenium.NewSeleniumService(seleniumPath, port, s.Opts...)
+	s.Instance, err = selenium.NewSeleniumService(seleniumPath, port, s.Opts...)
 	if err != nil {
 		log.Fatal(err) // Fatal error, exit if webdriver can't be initialize.
 	}
-	defer service.Stop()
+}
+
+// InitFirefoxWebDriver init and launch web driver with Firefox
+func (s *Selenium) InitFirefoxWebDriver() {
+	caps := selenium.Capabilities{"browserName": "firefox"}
+	s.WebDriver, err = selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+// CloseSelenium close webdriver and selenium instance
+func (s *Selenium) CloseSelenium() {
+	s.Instance.Stop()
+	if s.WebDriver != nil {
+		s.WebDriver.Quit()
+	}
 }
