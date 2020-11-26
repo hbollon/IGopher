7a17ad9d9974e2cb837d6512d68f83e2ecab1827
Hugo Bollon <hugo.bollon@gmail.com>
2020-11-26 16:43:17 +0100

    Added GetElement and IsElementPresent methods to webdriver engine

--- engine.go
+++ engine.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/tebeka/selenium"
@@ -53,5 +55,34 @@ func (s *Selenium) CloseSelenium() {
 	s.Instance.Stop()
 	if s.WebDriver != nil {
 		s.WebDriver.Quit()
+	}
+}
+
+/* Browser methods */
+
+// IsElementPresent check if an element is present on the current webpage
+func (s *Selenium) IsElementPresent(by, value string) bool {
+	_, err := s.WebDriver.FindElement(by, value)
+	if err != nil {
+		log.Infof("Element not found by %s: %v", by, err)
+		return false
+	}
+	return true
+}
+
+// GetElement wait for element and then return when it is available
+func (s *Selenium) GetElement(elementTag, locator string) (selenium.WebElement, error) {
+	locator = strings.ToUpper(locator)
+	if locator == "ID" && s.IsElementPresent(selenium.ByID, elementTag) {
+		return s.WebDriver.FindElement(selenium.ByID, elementTag)
+	} else if locator == "NAME" && s.IsElementPresent(selenium.ByName, elementTag) {
+		return s.WebDriver.FindElement(selenium.ByName, elementTag)
+	} else if locator == "XPATH" && s.IsElementPresent(selenium.ByXPATH, elementTag) {
+		return s.WebDriver.FindElement(selenium.ByXPATH, elementTag)
+	} else if locator == "CSS" && s.IsElementPresent(selenium.ByCSSSelector, elementTag) {
+		return s.WebDriver.FindElement(selenium.ByCSSSelector, elementTag)
+	} else {
+		log.Debugf("Incorrect locator '%s'", locator)
+		return nil, errors.New("Incorrect locator")
 	}
 }
