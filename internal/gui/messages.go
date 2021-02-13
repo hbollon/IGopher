c8fddddf88125ae10b1e495ad188c34b7eed251b
Hugo Bollon <hugo.bollon@gmail.com>
2021-02-13 16:27:05 +0100

    Change messages structure with json format

--- internal/gui/messages.go
+++ internal/gui/messages.go
@@ -1,22 +1,44 @@
 package gui
 
 import (
+	"encoding/json"
+
 	"github.com/asticode/go-astilectron"
 	"github.com/hbollon/igopher"
+	"github.com/sirupsen/logrus"
 )
+
+// MessageOut represents a message going out
+type MessageOut struct {
+	Msg     string      `json:"msg"`
+	Payload interface{} `json:"payload,omitempty"`
+}
+
+// MessageIn represents a message going in
+type MessageIn struct {
+	Msg     string          `json:"msg"`
+	Payload json.RawMessage `json:"payload,omitempty"`
+}
 
 func handleMessages(w *astilectron.Window) {
 	w.OnMessage(func(m *astilectron.EventMessage) interface{} {
 		// Unmarshal
-		var s string
-		m.Unmarshal(&s)
+		var i MessageIn
+		var err error
+		if err = m.Unmarshal(&i); err != nil {
+			logrus.Errorf("Unmarshaling message %+v failed: %v", *m, err)
+			return MessageOut{Msg: "Error during message reception"}
+		}
 
 		// Process message
-		if s == "resetGlobalDefaultSettings" {
+		switch i.Msg {
+		case "resetGlobalDefaultSettings":
 			config := igopher.ResetBotConfig()
 			igopher.ExportConfig(config)
-			return "Global settings successfully reset!"
+			return MessageOut{Msg: "Global settings successfully reset!"}
+		default:
+			logrus.Error("Unexpected message received.")
+			return MessageOut{Msg: "Error: Unexpected message received."}
 		}
-		return nil
 	})
 }
