1af4d98cf240ad722d1317ec60315a17e8f48731
Hugo Bollon <hugo.bollon@gmail.com>
2021-02-27 23:39:47 +0100

    Fix selenium hot reload loading animation + Add selenium close on bot exit

--- bot.go
+++ bot.go
@@ -270,6 +270,7 @@ func launchDmBot(ctx context.Context) {
 			return
 		case <-ctx.Done():
 			BotStruct.exitCh <- true
+			BotStruct.SeleniumStruct.CloseSelenium()
 			exitedCh <- true
 			BotStruct.running = false
 			return
