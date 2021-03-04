9f5216b4babf82601b137786d5f45c891b0c6638
Hugo Bollon <hugo.bollon@gmail.com>
2021-03-04 15:45:30 +0100

    Add bundler.sh script used to build and generate gui executables

---
+++ scripts/bundle.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script used to build GUI version and bundle executables for Windows/Linux/MacOS
+
+# Check if pwd is located inside the SkillsList folder
+if [ $(basename "$PWD") != "gui-bundler" ]; then
+    cd cmd/igopher/gui-bundler
+    if [ $(basename "$PWD") != "gui-bundler" ]; then
+        echo "Invalid current directory! Please cd to the IGopher directory and re-run this script."
+        exit 1
+    fi
+fi
+
+# Download and install go-astilectron-bundler
+go get -u github.com/asticode/go-astilectron-bundler/...
+go install github.com/asticode/go-astilectron-bundler/astilectron-bundler
+
+# Rename default bind.go to tmp file 
+mv bind.go bind.go.tmp
+
+# Execute astilectron-bundler
+astilectron-bundler -c bundler.json
+
+# Delete generated files and restore bind.go
+rm bind_*.go windows.syso
+mv bind.go.tmp bind.go
+echo "Done. Executables are located in 'cmd/igopher/gui-bundler/output/' folder"
