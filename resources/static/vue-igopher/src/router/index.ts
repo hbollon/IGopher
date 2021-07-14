6f42449fa80277e7a760b45ffb513bd488d4a375
Hugo Bollon <hugo.bollon@gmail.com>
2021-07-14 14:28:07 +0200

    feat(vuejs): view for 404 errors and router configuration

--- resources/static/vue-igopher/src/router/index.ts
+++ resources/static/vue-igopher/src/router/index.ts
@@ -3,6 +3,7 @@ import DmAutomation from '../views/DmAutomation.vue'
 import Settings from '../views/Settings.vue'
 import Logs from '../views/Logs.vue'
 import About from '../views/About.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -24,6 +25,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: '404 Not Found',
+    component: NotFound
   }
 ]
 
---
+++ resources/static/vue-igopher/src/views/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid">
+    <div class="text-center mt-5">
+        <div class="error mx-auto" data-text="404">
+            <p class="m-0">404</p>
+        </div>
+        <p class="text-dark mb-5 lead">Page Not Found</p>
+        <p class="text-black-50 mb-0">It looks like you found a glitch in the matrix...</p><router-link class="nav-link" to="/">← Back to Dashboard</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Vue, Options } from "vue-class-component";
+
+@Options({
+  title: "404 Not Found",
+})
+export default class NotFound extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
